98e31177f10da9aa26861eeccf3597c9e078b7cb
mausor11 <bartosz.danko@gmail.com>
2023-07-31 23:56:58 +0200

    pomodoro scene almost done

--- src/main/java/org/main/PomodoroController.java
+++ src/main/java/org/main/PomodoroController.java
@@ -5,9 +5,12 @@ import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.shape.Arc;
 import javafx.scene.shape.Circle;
 import javafx.util.Duration;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PomodoroController {
     @FXML
@@ -22,10 +25,14 @@ public class PomodoroController {
     Circle thirdP;
     @FXML
     Circle fourthP;
+    @FXML
+    Label clock;
     private int pomodoroCount = 0;
     private boolean isFirst = false;
+    private final int pomodoroTime = 10;
 
     public void initialize() {
+        setUpTimer(pomodoroTime);
     }
     public void startTimer(double time) {
         if(pomodoroCount == 0 && isFirst) {
@@ -45,11 +52,13 @@ public class PomodoroController {
                         );
                         timeline.play();
                         timeline.setOnFinished((finish) -> {
+                            setUpTimer(pomodoroTime);
                             timerButton.setDisable(false);
                             timerLook.setLength(360);
                             timerButton.setStyle("-fx-background-color: #f4f1f0");
                             pomodoroCount++;
                             switchDone();
+
                         });
             });
         } else {
@@ -61,6 +70,7 @@ public class PomodoroController {
             );
             timeline.play();
             timeline.setOnFinished((finish) -> {
+                setUpTimer(pomodoroTime);
                 timerButton.setDisable(false);
                 timerLook.setLength(360);
                 timerButton.setStyle("-fx-background-color: #f4f1f0");
@@ -96,7 +106,28 @@ public class PomodoroController {
         }
     }
     public void startClock() {
-        startTimer(1);
+        startTime(pomodoroTime);
+        startTimer(pomodoroTime);
+    }
+    public void startTime(int time) {
+        AtomicInteger k = new AtomicInteger(time - 1);
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            setUpTimer(k.get());
+            k.getAndDecrement();
+        }));
+
+        timeline.setCycleCount(time);
+        timeline.play();
+    }
+    public void setUpTimer(int time) {
+        int min = time / 60;
+        int sec = time % 60;
+        if(sec < 10) {
+            clock.setText(min + ":0" + sec);
+        } else {
+            clock.setText(min + ":" + sec);
+        }
     }
 
 }
