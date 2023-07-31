5290c3a2b72dc0fb3f4b5f8364496be8ce5dd009
mausor11 <bartosz.danko@gmail.com>
2023-07-31 22:56:48 +0200

    small

--- src/main/java/org/main/PomodoroController.java
+++ src/main/java/org/main/PomodoroController.java
@@ -1,10 +1,8 @@
 package org.main;
 
-import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.shape.Arc;
