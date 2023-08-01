e5f6bb259cea3b2a0f4d8a43235ec4b4ae0b2572
mausor11 <bartosz.danko@gmail.com>
2023-08-01 21:03:31 +0200

    settings done

--- src/main/java/org/main/PomodoroController.java
+++ src/main/java/org/main/PomodoroController.java
@@ -1,6 +1,5 @@
 package org.main;
 
-import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
@@ -10,7 +9,6 @@ import javafx.scene.control.Label;
 import javafx.scene.shape.Arc;
 import javafx.scene.shape.Circle;
 import javafx.util.Duration;
-
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class PomodoroController {
@@ -30,25 +28,30 @@ public class PomodoroController {
     Label clock;
     private int pomodoroCount = 0;
     private boolean isFirst = false;
-    public static int pomodoroTime = 1800;
+    private boolean isBreak = false;
+    private boolean isLongBreak = false;
+    public static int pomodoroTime = 1500;
+    public static int pomodoroBreak = 300;
+    public static int pomodoroLongBreak = 600;
 
     public void initialize() {
         clock.setText(PomodoroController.pomodoroTime / 60 + ":00");
+
         SettingsPomodoro.actTime.textProperty().addListener((observable, oldValue, newValue) -> {
-            PomodoroController.pomodoroTime = 60 * Integer.parseInt(newValue);
+            PomodoroController.pomodoroTime = Integer.parseInt(newValue);
             setUpTimer(pomodoroTime);
+        });
+
+        SettingsPomodoro.actBreak.textProperty().addListener((observable, oldValue, newValue) -> {
+            PomodoroController.pomodoroBreak = Integer.parseInt(newValue);
+        });
+
+        SettingsPomodoro.actLongBreak.textProperty().addListener((observable, oldValue, newValue) -> {
+            PomodoroController.pomodoroLongBreak = Integer.parseInt(newValue);
         });
     }
     public void startTimer(double time) {
         if(pomodoroCount == 0 && isFirst) {
-            Timeline animation = new Timeline(
-                    new KeyFrame(Duration.millis(200), new KeyValue(firstP.styleProperty(), "-fx-fill: #f4f1f0")),
-                    new KeyFrame(Duration.millis(150), new KeyValue(secondP.styleProperty(), "-fx-fill: #f4f1f0")),
-                    new KeyFrame(Duration.millis(100), new KeyValue(thirdP.styleProperty(), "-fx-fill: #f4f1f0")),
-                    new KeyFrame(Duration.millis(50), new KeyValue(fourthP.styleProperty(), "-fx-fill: #f4f1f0"))
-            );
-            animation.play();
-            animation.setOnFinished((d) -> {
                         timerButton.setDisable(true);
                         timerButton.setStyle("-fx-background-color: #efe8e7");
                         Timeline timeline = new Timeline(
@@ -58,14 +61,15 @@ public class PomodoroController {
                         timeline.play();
                         timeline.setOnFinished((finish) -> {
                             setUpTimer(pomodoroTime);
-                            timerButton.setDisable(false);
-                            timerLook.setLength(360);
+                            resetTimer();
                             timerButton.setStyle("-fx-background-color: #f4f1f0");
                             pomodoroCount++;
                             switchDone();
+                            isBreak = true;
+                            timerLook.setStyle("-fx-fill: #fa5e42");
+                            clock.setText(PomodoroController.pomodoroBreak / 60 + ":00");
 
                         });
-            });
         } else {
             timerButton.setDisable(true);
             timerButton.setStyle("-fx-background-color: #efe8e7");
@@ -76,23 +80,63 @@ public class PomodoroController {
             timeline.play();
             timeline.setOnFinished((finish) -> {
                 setUpTimer(pomodoroTime);
-                timerButton.setDisable(false);
-                timerLook.setLength(360);
+                resetTimer();
                 timerButton.setStyle("-fx-background-color: #f4f1f0");
                 pomodoroCount++;
 
-
                 if (pomodoroCount == 4) {
+                    isLongBreak = true;
+                    timerLook.setStyle("-fx-fill: #3165a0");
+                    clock.setText(PomodoroController.pomodoroLongBreak / 60 + ":00");
                     switchDone();
                     isFirst = true;
                     pomodoroCount = 0;
                 } else {
                     switchDone();
+                    isBreak = true;
+                    timerLook.setStyle("-fx-fill: #fa5e42");
+                    clock.setText(PomodoroController.pomodoroBreak / 60 + ":00");
 
                 }
             });
         }
-
+    }
+    public void startBreak(double time) {
+        timerButton.setDisable(true);
+        timerButton.setStyle("-fx-background-color: #efe8e7");
+        timerLook.setStyle("-fx-fill: #fa5e42");
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.ZERO, new KeyValue(timerLook.lengthProperty(), 360)),
+                new KeyFrame(Duration.seconds(time), new KeyValue(timerLook.lengthProperty(), 0))
+        );
+        timeline.play();
+        timeline.setOnFinished((finish) -> {
+            isBreak = false;
+            timerLook.setStyle("-fx-fill: #946057");
+            resetTimer();
+        });
+    }
+    public void startLongBreak(double time) {
+        timerButton.setDisable(true);
+        timerButton.setStyle("-fx-background-color: #efe8e7");
+        timerLook.setStyle("-fx-fill: #3165a0");
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.ZERO, new KeyValue(timerLook.lengthProperty(), 360)),
+                new KeyFrame(Duration.seconds(time), new KeyValue(timerLook.lengthProperty(), 0))
+        );
+        timeline.play();
+        timeline.setOnFinished((finish) -> {
+            isLongBreak = false;
+            timerLook.setStyle("-fx-fill: #946057");
+            resetTimer();
+            Timeline animation = new Timeline(
+                    new KeyFrame(Duration.millis(200), new KeyValue(firstP.styleProperty(), "-fx-fill: #f4f1f0")),
+                    new KeyFrame(Duration.millis(150), new KeyValue(secondP.styleProperty(), "-fx-fill: #f4f1f0")),
+                    new KeyFrame(Duration.millis(100), new KeyValue(thirdP.styleProperty(), "-fx-fill: #f4f1f0")),
+                    new KeyFrame(Duration.millis(50), new KeyValue(fourthP.styleProperty(), "-fx-fill: #f4f1f0"))
+            );
+            animation.play();
+        });
     }
     public void switchDone() {
         switch(pomodoroCount) {
@@ -111,8 +155,17 @@ public class PomodoroController {
         }
     }
     public void startClock() {
-        startTime(PomodoroController.pomodoroTime);
-        startTimer(PomodoroController.pomodoroTime);
+        if(isBreak) {
+            startBreak(PomodoroController.pomodoroBreak);
+            startTime(PomodoroController.pomodoroBreak);
+        } else if(isLongBreak) {
+            startLongBreak(PomodoroController.pomodoroLongBreak);
+            startTime(PomodoroController.pomodoroLongBreak);
+        } else {
+            startTime(PomodoroController.pomodoroTime);
+            startTimer(PomodoroController.pomodoroTime);
+        }
+
     }
     public void startTime(int time) {
         AtomicInteger k = new AtomicInteger(time - 1);
@@ -132,6 +185,16 @@ public class PomodoroController {
         } else {
             clock.setText(min + ":" + sec);
         }
+    }
+    public void resetTimer() {
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.ZERO, new KeyValue(timerLook.lengthProperty(), 0)),
+                new KeyFrame(Duration.millis(200), new KeyValue(timerLook.lengthProperty(), 360))
+        );
+        timeline.play();
+        timeline.setOnFinished((finish) -> {
+            timerButton.setDisable(false);
+        });
     }
 
 }
--- src/main/java/org/main/SettingsPomodoro.java
+++ src/main/java/org/main/SettingsPomodoro.java
@@ -5,27 +5,91 @@ import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.shape.Rectangle;
 
 public class SettingsPomodoro {
     @FXML
-    Slider timeSlider;
+    Slider workSlider;
     @FXML
-    Label time;
+    Slider breakSlider;
+    @FXML
+    Slider longBreakSlider;
+    @FXML
+    Label workTime;
+    @FXML
+    Label breakTime;
+    @FXML
+    Label longBreakTime;
+    @FXML
+    Rectangle progressBarWork;
+    @FXML
+    Rectangle progressBarBreak;
+    @FXML
+    Rectangle progressBarLongBreak;
     public static Label actTime = new Label();
+    public static Label actBreak = new Label();
+    public static Label actLongBreak = new Label();
     public void initialize() {
-        timeSlider.setValue(PomodoroController.pomodoroTime / 60);
-        time.setText(PomodoroController.pomodoroTime / 60 + ":00");
-        timeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+        progressBarWork.setDisable(true);
+        progressBarBreak.setDisable(true);
+        progressBarLongBreak.setDisable(true);
+
+        workSlider.setValue(PomodoroController.pomodoroTime / 60.0);
+        breakSlider.setValue(PomodoroController.pomodoroBreak / 60.0);
+        longBreakSlider.setValue(PomodoroController.pomodoroLongBreak / 60.0);
+
+        double percentage1 = ((PomodoroController.pomodoroTime / 60.0)  - 1) / (workSlider.getMax() - 1);
+        double percentage2 = ((PomodoroController.pomodoroBreak / 60.0) - 1) / (breakSlider.getMax() - 1);
+        double percentage3 = ((PomodoroController.pomodoroLongBreak / 60.0) - 1) / (longBreakSlider.getMax() - 1);
+
+        progressBarWork.setWidth(366 * percentage1);
+        progressBarBreak.setWidth(366 * percentage2);
+        progressBarLongBreak.setWidth(366 * percentage3);
+
+        workTime.setText(PomodoroController.pomodoroTime / 60 + ":00");
+        breakTime.setText(PomodoroController.pomodoroBreak / 60 + ":00");
+        longBreakTime.setText(PomodoroController.pomodoroLongBreak / 60 + ":00");
+
+        workSlider.valueProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
-                PomodoroController.pomodoroTime = newValue.intValue();
-                setUpTimer(newValue.intValue());
-                actTime.setText(newValue.intValue() + "");
+                double percentage = (newValue.doubleValue()-1)  / (workSlider.getMax() -1);
+                progressBarWork.setWidth(366 * percentage);
+                PomodoroController.pomodoroTime = newValue.intValue() * 60;
+                setUpTimerWork(newValue.intValue());
+                actTime.setText(newValue.intValue()*60 + "");
             }
         });
+        breakSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
+                double percentage = (newValue.doubleValue()-1)  / (breakSlider.getMax() -1);
+                progressBarBreak.setWidth(366 * percentage);
+                PomodoroController.pomodoroBreak = newValue.intValue() * 60;
+                setUpTimerBreak(newValue.intValue());
+                actBreak.setText(newValue.intValue()*60 + "");
+            }
+        });
+        longBreakSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
+                double percentage = (newValue.doubleValue()-1)  / (longBreakSlider.getMax() -1);
+                progressBarLongBreak.setWidth(366 * percentage);
+                PomodoroController.pomodoroLongBreak = newValue.intValue() * 60;
+                setUpTimerLongBreak(newValue.intValue());
+                actLongBreak.setText(newValue.intValue()*60 + "");
+            }
+        });
+
     }
-    public void setUpTimer(int time) {
-        this.time.setText(time + ":00");
+    public void setUpTimerWork(int time) {
+        workTime.setText(time + ":00");
+    }
+    public void setUpTimerBreak(int time) {
+        breakTime.setText(time + ":00");
+    }
+    public void setUpTimerLongBreak(int time) {
+        longBreakTime.setText(time + ":00");
     }
 
 }
